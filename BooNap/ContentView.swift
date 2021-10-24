c28976d9e5063e891ce961c9f7dd84a732e379bb
jamil raai <jamilraai@MBPvonBenedikt.fritz.box>
2021-10-24 12:29:00 +0200

    AnimationView added

--- BooNap/ContentView.swift
+++ BooNap/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
                                                        "211009-boonap-proto")
     @State private var contentOffset: CGPoint = .zero
     @State private var currentYCord : CGFloat = 0
-    @State private var waterLevel = WaterLevel.SunLevel
+    @State private var waterLevel = enumWaterLevel.SunLevel
     @State private var lottieViewList = [LottieView]()
     @State var lottieViewIndex = 0
     
@@ -26,62 +26,38 @@ struct ContentView: View {
 
     @State private var opacity : Double = 0
     
+    /// WaterLevel Object
+    @ObservedObject var WaterLevelObject = WaterLevel(displaySize: UIScreen.main.bounds)
+    
     init(){
         UIScrollView.appearance().bounces = false
     }
     
     var body: some View {
-        
-        ScrollViewReader { scrollView in
-            ZStack(alignment: .bottom) {
-                ScrollableView(self.$contentOffset, animationDuration: 2.5){
-
-
-//                    let view : LottieView = LottieView(filename: animationFileName[0], animationSpeed: 1, loopMode: .loop)
-//
-//                    let bobbelView : LottieView = LottieView(filename: animationFileName[1], animationSpeed: 0.5, loopMode: .playOnce)
-//
-//                    getSunLevelView(scrollViewproxy: scrollView,view: view,bobbelView: bobbelView )
-//                        .onAppear(perform: {
-//                            view.play()
-//                            bobbelView.playToFrame(toFrame: 25)
-//                        })
-//
-//
-//                    let waterView : LottieView = LottieView(filename: animationFileName[2], animationSpeed: 1, loopMode: .loop)
-//
-//                    getFirstWaterLevelView(waterView: waterView)
-//
-//                    let booView : LottieView = LottieView(filename: animationFileName[3], animationSpeed: 1, loopMode: .loop)
-//
-//                    booView.aspectRatio(contentMode: .fill)
-//                       .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-//                        .cornerRadius(6.0)
-//                        .onAppear(perform: {
-//                            lottieViewList.insert(view, at: 0)
-//                            lottieViewList.insert(bobbelView, at: 1)
-//                            lottieViewList.insert(waterView, at: 2)
-//                            lottieViewList.insert(booView, at: 3)
-//                        })
+        ZStack(alignment: .bottom) {
+            ZStack {
                     let view : LottieView = LottieView(filename: animationFileName[0], animationSpeed: 1, loopMode: .loop)
 
                     let bobbelView : LottieView = LottieView(filename: animationFileName[1], animationSpeed: 0.5, loopMode: .playOnce)
 
+                    getSunLevelView(view: view,bobbelView: bobbelView )
+                        .offset(x: 0, y: WaterLevelObject.OffSetSunLevelView)
+                        .onAppear(perform: {
+                            view.play()
+                            bobbelView.playToFrame(toFrame: 25)
+                        })
+
+
                     let waterView : LottieView = LottieView(filename: animationFileName[2], animationSpeed: 1, loopMode: .loop)
 
-                    getSunLevelView(scrollViewproxy: scrollView,view: view,bobbelView: bobbelView )
-                            .onAppear(perform: {
-                                view.play()
-                                bobbelView.playToFrame(toFrame: 25)
-                            })
-
                     getFirstWaterLevelView(waterView: waterView)
-                        .id(2)
+                        .offset(x: 0, y: WaterLevelObject.OffSetMidWaterLevelView)
 
                     let booView : LottieView = LottieView(filename: animationFileName[3], animationSpeed: 1, loopMode: .loop)
 
                     booView.aspectRatio(contentMode: .fill)
-                       .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                        .offset(x: 0, y: WaterLevelObject.OffSetDeepWaterLevelView)
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
                         .cornerRadius(6.0)
                         .onAppear(perform: {
                             lottieViewList.insert(view, at: 0)
@@ -90,118 +66,56 @@ struct ContentView: View {
                             lottieViewList.insert(booView, at: 3)
                         })
                 }
-                .background(Color.init("AccentColor"))
-                .ignoresSafeArea(.all)
+            ZStack {
+                Rectangle()
+                    .frame(width: UIScreen.main.bounds.width, height: 50)
+                    .foregroundColor(.blue.opacity(0.2))
+                    .onTapGesture(perform: {
+                        withAnimation(.easeOut(duration: 2.5)){
+                            let smaleView = lottieViewList[1]
+                            var view = lottieViewList[waterLevel.rawValue]
+                            view.stop()
+                            lottieViewList[waterLevel.rawValue] = view
 
-                ZStack {
-                    Rectangle()
-                        .frame(width: UIScreen.main.bounds.width, height: 50)
-                        .foregroundColor(.blue.opacity(0.2))
-                        .onTapGesture(perform: {
-                            withAnimation(.easeOut){
-                                let smaleView = lottieViewList[1]
-                                var view = lottieViewList[waterLevel.rawValue]
-                                view.stop()
-                                lottieViewList[waterLevel.rawValue] = view
-
-                                switch waterLevel {
-                                    case .SunLevel:
-                                        waterLevel = .FirstWaterLevel
-                                        smaleView.play()
-                                        self.currentYCord += UIScreen.main.bounds.height + 10
-
-                                        self.contentOffset = CGPoint(x: 0, y: currentYCord)
-                                    case .FirstWaterLevel:
-                                        waterLevel = .DeepWaterLevel
-                                        self.currentYCord += UIScreen.main.bounds.height + 10
-
-                                        self.contentOffset = CGPoint(x: 0, y: currentYCord)
-                                    case .DeepWaterLevel:
-                                        waterLevel = .SunLevel
-                                        smaleView.playToFrame(toFrame: 25)
-                                        self.currentYCord = 0
-                                        self.contentOffset = CGPoint(x: 0, y: currentYCord)
-                                }
-
-                                view = lottieViewList[waterLevel.rawValue]
-                                view.play()
-                                lottieViewList[waterLevel.rawValue] = view
+                            switch waterLevel {
+                                case .SunLevel:
+                                    waterLevel = .MidWaterLevel
+                                    smaleView.play()
+                                    self.currentYCord += UIScreen.main.bounds.height + 10
+//
+                                    self.WaterLevelObject.OffSetSunLevelView = -currentYCord
+                                    self.WaterLevelObject.OffSetMidWaterLevelView = 0
+                                case .MidWaterLevel:
+                                    waterLevel = .DeepWaterLevel
+                                    self.currentYCord += UIScreen.main.bounds.height + 10
+                                    
+                                    self.WaterLevelObject.OffSetSunLevelView += -currentYCord
+                                    self.WaterLevelObject.OffSetMidWaterLevelView = -currentYCord
+                                    self.WaterLevelObject.OffSetDeepWaterLevelView = 0
+                                case .DeepWaterLevel:
+                                    waterLevel = .SunLevel
+                                    smaleView.playToFrame(toFrame: 25)
+                                    self.currentYCord = 0
+                                    self.WaterLevelObject.OffSetSunLevelView = 0
+                                    self.WaterLevelObject.OffSetMidWaterLevelView = UIScreen.main.bounds.height
+                                    self.WaterLevelObject.OffSetDeepWaterLevelView = UIScreen.main.bounds.height * 2
                             }
-                        })
-                    Image(systemName: waterLevel == .DeepWaterLevel ? "arrow.up" : "arrow.down")
-                        .font(.largeTitle)
-                        .aspectRatio(contentMode: .fill)
 
-                }
-            }.ignoresSafeArea(.all)
+                            view = lottieViewList[waterLevel.rawValue]
+                            view.play()
+                            lottieViewList[waterLevel.rawValue] = view
+                        }
+                    })
+                Image(systemName: waterLevel == .DeepWaterLevel ? "arrow.up" : "arrow.down")
+                    .font(.largeTitle)
+                    .aspectRatio(contentMode: .fill)
 
-        }
+            }
+        }.ignoresSafeArea(.all)
         
-//        ScrollViewReader { scrollView in
-//            ScrollView(showsIndicators: false) {
-//                let view : LottieView = LottieView(filename: animationFileName[0], animationSpeed: 1, loopMode: .loop)
-//
-//                let bobbelView : LottieView = LottieView(filename: animationFileName[1], animationSpeed: 0.5, loopMode: .playOnce)
-//
-//                let waterView : LottieView = LottieView(filename: animationFileName[2], animationSpeed: 1, loopMode: .loop)
-//
-//                getSunLevelView(scrollViewproxy: scrollView,view: view,bobbelView: bobbelView )
-//                        .onAppear(perform: {
-//                            view.play()
-//                            bobbelView.playToFrame(toFrame: 25)
-//                        })
-//
-//                getFirstWaterLevelView(waterView: waterView)
-//                    .id(2)
-//
-//                let booView : LottieView = LottieView(filename: animationFileName[3], animationSpeed: 1, loopMode: .loop)
-//
-//                booView.aspectRatio(contentMode: .fill)
-//                   .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-//                    .cornerRadius(6.0)
-//                    .onAppear(perform: {
-//                        booView.play()
-//                        self.lottieViewList.append(booView)
-//                    })
-//            }
-//            .background(Color.init("AccentColor"))
-//            .ignoresSafeArea(.all)
-//            .onTapGesture(perform: {
-//                withAnimation(.easeIn){
-////                    let smaleView = lottieViewList[1]
-////                    var view = lottieViewList[waterLevel.rawValue]
-////                    view.stop()
-////                    lottieViewList[waterLevel.rawValue] = view
-////
-////                    switch waterLevel {
-////                        case .SunLevel:
-////                            waterLevel = .FirstWaterLevel
-////                            smaleView.play()
-////                            self.currentYCord += UIScreen.main.bounds.height + 10
-////
-////                            self.contentOffset = CGPoint(x: 0, y: currentYCord)
-////                        case .FirstWaterLevel:
-////                            waterLevel = .DeepWaterLevel
-////                            self.currentYCord += UIScreen.main.bounds.height + 10
-////
-////                            self.contentOffset = CGPoint(x: 0, y: currentYCord)
-////                        case .DeepWaterLevel:
-////                            waterLevel = .SunLevel
-////                            smaleView.playToFrame(toFrame: 25)
-////                            self.currentYCord = 0
-////                            self.contentOffset = CGPoint(x: 0, y: currentYCord)
-////                    }
-////
-////                    view = lottieViewList[waterLevel.rawValue]
-////                    view.play()
-////                    lottieViewList[waterLevel.rawValue] = view
-////                    scrollView.scrollTo(2)
-//                }
-//            })
-//        }
     }
     
-    func getSunLevelView(scrollViewproxy : ScrollViewProxy, view : LottieView, bobbelView : LottieView) -> some View {
+    func getSunLevelView(view : LottieView, bobbelView : LottieView) -> some View {
         
         return ZStack {
 
@@ -224,9 +138,6 @@ struct ContentView: View {
                             })
                         
                         Button(action: {
-                            withAnimation(.easeOut(duration: 2.5)){
-                                scrollViewproxy.scrollTo(2)
-                            }
                         }, label: {
                             Text("Let's Dive in ...")
                                 .frame(width: 250, height: 150)
@@ -254,54 +165,54 @@ struct ContentView: View {
     
     func getFirstWaterLevelView(waterView : LottieView ) -> some View {
         return ZStack {
-            Color("Test")
             
-            Rectangle()
-                .foregroundColor(Color.init("AccentColor"))
-                .overlay(
-                    waterView.aspectRatio(contentMode: .fill)
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                         .cornerRadius(6.0)
-                )
-                .background(Color.white)
-                .clipShape(LiquidSwipe(offset: offset))
-                .ignoresSafeArea(.all)
-                .overlay(
-                    Image(systemName: "chevron.left")
-                        .font(.largeTitle)
-                        .frame(width: 50, height: 50)
-                        .contentShape(Rectangle())
-                        .gesture(DragGesture().onChanged({ (value) in
-                            // animation Swipe offset...
-                            withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.6, blendDuration: 0.6)) {
-                                offset = value.translation
-                            }
-                        }).onEnded({ (value) in
-                            let screen = UIScreen.main.bounds
-                            withAnimation(.spring()) {
-                                if -offset.width > screen.width / 2 {
-                                    offset.width = -screen.height
-                                    ShowSettings.toggle()
-                                } else {
-                                    offset = .zero
+                Color("Test")
+                
+                Rectangle()
+                    .foregroundColor(Color.init("AccentColor"))
+                    .overlay(
+                        waterView.aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                             .cornerRadius(6.0)
+                    )
+                    .background(Color.white)
+                    .clipShape(LiquidSwipe(offset: offset))
+                    .ignoresSafeArea(.all)
+                    .overlay(
+                        Image(systemName: "chevron.left")
+                            .font(.largeTitle)
+                            .frame(width: 50, height: 50)
+                            .contentShape(Rectangle())
+                            .gesture(DragGesture().onChanged({ (value) in
+                                // animation Swipe offset...
+                                withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.6, blendDuration: 0.6)) {
+                                    offset = value.translation
                                 }
+                            }).onEnded({ (value) in
+                                let screen = UIScreen.main.bounds
+                                withAnimation(.spring()) {
+                                    if -offset.width > screen.width / 2 {
+                                        offset.width = -screen.height
+                                        ShowSettings.toggle()
+                                    } else {
+                                        offset = .zero
+                                    }
+                                }
+                            }))
+                            .offset(x: 10, y: 100)
+                            .opacity(offset == .zero ? 1 : 0)
+                        , alignment: .topTrailing
+                    )
+                
+                if ShowSettings {
+                    Image("Vertical")
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                offset = .zero
+                                ShowSettings.toggle()
                             }
-                        }))
-                        .offset(x: 10, y: 100)
-                        .opacity(offset == .zero ? 1 : 0)
-                    , alignment: .topTrailing
-                )
-            
-            if ShowSettings {
-                Image("Vertical")
-                    .onTapGesture {
-                        withAnimation(.spring()) {
-                            offset = .zero
-                            ShowSettings.toggle()
                         }
-                    }
-            }
-            
+                }
         }
         .background(Color.blue)
         .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
@@ -312,10 +223,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-enum WaterLevel : Int {
-    case SunLevel = 0
-    case FirstWaterLevel = 2
-    case DeepWaterLevel = 3
 }
